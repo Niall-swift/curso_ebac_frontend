eeff91ab649ab4540fb5c106d17b96497fbc91d1
Josué Ramos <niallswiftnerd@outlook.com>
2023-09-08 10:22:49 -0300

    exercicio_jquery_plugn

---
+++ index.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('#Telefone').mask('(00) 00000-0000');
+    $('#Cpf').mask('000.000.000-00');
+    $('#Cep').mask('00000-000');
+    $('#Numero').mask('N° 0000');
+})
